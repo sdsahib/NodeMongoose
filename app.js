b73a057903ab7268100d590d02b2892cb5342bd6
Sahib Singh <sahisingh@teksystems.com>
2018-09-06 19:07:18 +0530

    Basic REST Controllers Implemented

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel');
 var app = express();
+var port = process.env.PORT || 3100;
 
-var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/Books', bookRouter);
+
+
+app.get('/', function (req, res) {
+    res.send('Api setup done');
+});
+
+app.listen(port, function () {
+    console.log('we are up on' + port);
+});
